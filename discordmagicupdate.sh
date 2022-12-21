131e5f734a69d5713b840b4f68a67940b4c3ece1
Akiko <sophie-bear@solsticegamestudios.com>
2022-12-21 14:58:54 -0500

    Add files via upload

--- discordmagicupdate.sh
+++ discordmagicupdate.sh
@@ -1,7 +1,8 @@
-cd ~/Downloads && 
-wget -O discord.tar.gz "https://discord.com/api/download?platform=linux&format=tar.gz" && 
-sudo tar --overwrite -xvzf discord.tar.gz -C /opt && 
-sudo ln -sf /opt/Discord/Discord /usr/bin/Discord && 
-sudo sed -i 's/Exec=\/usr\/share\/discord\/Discord/Exec=\/usr\/bin\/Discord/g' /opt/Discord/discord.desktop && 
-sudo sed -i 's/Icon=discord/Icon=\/opt\/Discord\/discord.png/g' /opt/Discord/discord.desktop && 
+cd ~/Downloads
+wget -O discord.tar.gz "https://discord.com/api/download?platform=linux&format=tar.gz"
+sudo tar --overwrite -xvzf discord.tar.gz -C /opt
+sudo ln -sf /opt/Discord/Discord /usr/bin/Discord
+sudo sed -i 's/Exec=\/usr\/share\/discord\/Discord/Exec=\/usr\/bin\/Discord/g' /opt/Discord/discord.desktop
+sudo sed -i 's/Icon=discord/Icon=\/opt\/Discord\/discord.png/g' /opt/Discord/discord.desktop
 sudo cp -r /opt/Discord/discord.desktop /usr/share/applications
+sudo rm -rf ~/Downloads/discord.tar.gz
