d805e937f5db4fb8c9e081bbb22c17864c4da778
Akiko <sophie-bear@solsticegamestudios.com>
2022-12-21 14:48:27 -0500

    Add files via upload

---
+++ discord.sh
@@ -0,0 +1,7 @@
+cd ~/Downloads && 
+wget -O discord.tar.gz "https://discord.com/api/download?platform=linux&format=tar.gz" && 
+sudo tar --overwrite -xvzf discord.tar.gz -C /opt && 
+sudo ln -sf /opt/Discord/Discord /usr/bin/Discord && 
+sudo sed -i 's/Exec=\/usr\/share\/discord\/Discord/Exec=\/usr\/bin\/Discord/g' /opt/Discord/discord.desktop && 
+sudo sed -i 's/Icon=discord/Icon=\/opt\/Discord\/discord.png/g' /opt/Discord/discord.desktop && 
+sudo cp -r /opt/Discord/discord.desktop /usr/share/applications
